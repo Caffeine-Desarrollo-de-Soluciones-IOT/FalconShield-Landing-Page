1230595ed712c4e6d716b865a2fd4a64fcfe6478
GabySol26 <U202113876@upc.edu.pe>
2024-10-15 12:27:57 -0500

    add:language switch and first section language change

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,58 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const texts = {
+    en: {
+        solutions: 'Solutions',
+        subscriptions: 'Subscription',
+        team: 'Work Team',
+        followers: 'Followers',
+        find: 'Find us here!',
+        about: 'About us',
+        title: 'Innovative Security for Your Property',
+        subtitle: 'Protect your home with advanced technology and effective solutions from \n Verysafe in Lima, Peru',
+        button: "Get Started"
+    },
+    es: {
+        solutions: 'Soluciones',
+        subscriptions: 'Suscripción',
+        team: 'Equipo de Trabajo',
+        followers: 'Seguidores',
+        find: '¡Encuéntranos aquí!',
+        about: 'Sobre nosotros',
+        title: 'Seguridad Innovadora para su Propiedad',
+        subtitle: 'Proteja su hogar con tecnología avanzada y soluciones efectivas de \n Verysafe en Lima, Perú',
+        button: "Empezar Ahora!"
+    }
+};
+
+// Selecciona el switch
+const languageSwitch = document.getElementById('language-switch');
+
+// Añadir un evento al switch para detectar cambios
+languageSwitch.addEventListener('change', function() {
+    if (this.checked) {
+        // Cambiar al español
+        document.getElementById('solutions-link').textContent = texts.es.solutions;
+        document.getElementById('subscriptions-link').textContent = texts.es.subscriptions;
+        document.getElementById('team-link').textContent = texts.es.team;
+        document.getElementById('followers-link').textContent = texts.es.followers;
+        document.getElementById('find-link').textContent = texts.es.find;
+        document.getElementById('about-link').textContent = texts.es.about;
+        document.getElementById('title-link').textContent = texts.es.title;
+        document.getElementById('subtitle-link').textContent = texts.es.subtitle;
+        document.getElementById('button-get-link').textContent = texts.es.button;
+    } else {
+        // Cambiar al inglés
+        document.getElementById('solutions-link').textContent = texts.en.solutions;
+        document.getElementById('subscriptions-link').textContent = texts.en.subscriptions;
+        document.getElementById('team-link').textContent = texts.en.team;
+        document.getElementById('followers-link').textContent = texts.en.followers;
+        document.getElementById('find-link').textContent = texts.en.find;
+        document.getElementById('about-link').textContent = texts.en.about;
+        document.getElementById('title-link').textContent = texts.en.title;
+        document.getElementById('subtitle-link').textContent = texts.en.subtitle;
+        document.getElementById('button-get-link').textContent = texts.en.button;
+    }
+});
